67e25f46d6114d4b7a49ec77bba65f10cca84996
therynamo <groetkentheryn@gmail.com>
2022-12-28 07:54:33 -0600

    mess around with home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,28 @@ import Layout from "@theme/Layout";
 
 import styles from "./index.module.css";
 
-export default function Home(): JSX.Element {
-  return <Layout description="Information about me - therynamo!"></Layout>;
+export default function Home() {
+  return (
+    <Layout description="Information about me - therynamo!">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          flexDirection: "column",
+          padding: "12px",
+        }}
+      >
+        <div>
+          <h1 className={styles.name}>Theryn</h1>
+          <p className={styles.labels}>Dad</p>
+          <p className={styles.labels}>Software Engineer</p>
+          <p className={styles.labels}>Aspiring Horticulturist</p>
+        </div>
+        <div>
+          <p className={styles.intro}>Hey. I do things!</p>
+        </div>
+      </div>
+    </Layout>
+  );
 }
